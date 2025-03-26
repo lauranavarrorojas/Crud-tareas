a2dd8d1e32653b9a00aa31c87be42b46038b71c7
Laura <sofianavarojas@emaill.com>
2025-03-26 18:18:37 -0500

    Actualizar configuracion y modelos

--- myapp/models.py
+++ myapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Project(models.Model):
@@ -12,6 +13,7 @@ class Task(models.Model):
     description = models.TextField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     done = models.BooleanField(default=False)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.title + ' - ' + self.project.name
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
-﻿/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './templates/**/*.html',
     './*/templates/**/*.html',
+    './myapp/templates/**/*.html',
     './static/**/*.js',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+} 
